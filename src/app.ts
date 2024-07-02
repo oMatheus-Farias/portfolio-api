b08f223c467ba30fe02875a4d8d2f8ce5ca5b001
Matheus <matheusfariasdasneves@gmail.com>
2024-07-02 18:53:30 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+import { Request, Response } from "express";
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Hello, world!" });
+});
