c73e39056d250290a2b64920768047463b8ea10f
Matheus <matheusfariasdasneves@gmail.com>
2024-07-06 09:31:37 -0300

    feat: add create user route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,27 @@
 import { Router, Request, Response } from "express"
+import { makeCreateUserController } from "./factories/controllers"
 
 export const router = Router()
 
 router.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Hello, world!" })
+  res.status(200).json({ message: "API is running 🚀" })
+})
+
+router.post("/api/user", async (req: Request, res: Response) => {
+  const createUserController = makeCreateUserController()
+
+  const { statusCode, body } = (await createUserController.execute({
+    httRequest: req.body,
+  })) as {
+    statusCode: number
+    body: {
+      id: string
+      firstName: string
+      lastName: string
+      email: string
+      password: string
+    }
+  }
+
+  res.status(statusCode).json(body)
 })
