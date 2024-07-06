4b0b197c7f59131bb847111a599a130f493b3b66
Matheus <matheusfariasdasneves@gmail.com>
2024-07-06 10:54:26 -0300

    fix: add body in errors http

--- src/errors/http.ts
+++ src/errors/http.ts
@@ -1,14 +1,20 @@
 export const badRequest = (message: string) => ({
   statusCode: 400,
-  message,
+  body: {
+    message,
+  },
 })
 
 export const internalServerError = (message: string) => ({
   statusCode: 500,
-  message,
+  body: {
+    message,
+  },
 })
 
 export const notFound = (message: string) => ({
   statusCode: 404,
-  message,
+  body: {
+    message,
+  },
 })
