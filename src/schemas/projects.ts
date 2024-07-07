57bed20583e34f5cfffdba61b261cc234f93e9ae
Matheus <matheusfariasdasneves@gmail.com>
2024-07-07 16:25:41 -0300

    feat: add createProjectsSchema

---
+++ src/schemas/projects.ts
@@ -0,0 +1,61 @@
+import { z } from "zod"
+
+export const createProjectsSchema = z.object({
+  name: z
+    .string({
+      required_error: "Name is required.",
+    })
+    .trim()
+    .min(1, {
+      message: "Name is required.",
+    })
+    .max(50, {
+      message: "Name must be at most 50 characters.",
+    }),
+  description: z
+    .string({
+      required_error: "Description is required.",
+    })
+    .trim()
+    .min(1, {
+      message: "Description is required.",
+    })
+    .max(500, {
+      message: "Description must be at most 500 characters.",
+    }),
+  imagesUrl: z.string().array().nonempty({
+    message: "Images URL is required.",
+  }),
+  repositoryUrl: z
+    .string({
+      required_error: "Repository URL is required.",
+    })
+    .url({
+      message: "Please provide a valid URL.",
+    })
+    .trim()
+    .min(1, {
+      message: "Repository URL is required.",
+    }),
+  projectUrl: z
+    .string({
+      required_error: "Repository URL is required.",
+    })
+    .url({
+      message: "Please provide a valid URL.",
+    })
+    .trim()
+    .min(1, {
+      message: "Repository URL is required.",
+    }),
+  technologies: z.string().array().nonempty({
+    message: "Technologies is required.",
+  }),
+  userId: z
+    .string({
+      required_error: "User ID is required.",
+    })
+    .uuid({
+      message: "User ID must be a valid UUID.",
+    }),
+})
