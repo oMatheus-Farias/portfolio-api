0698015de9cb11b2ca988224183640b53de2f2b9
Matheus <matheusfariasdasneves@gmail.com>
2024-07-07 18:54:40 -0300

    feat: add GetProjectByIdController

---
+++ src/controllers/projects/get-project-by-id.ts
@@ -0,0 +1,47 @@
+import { GetProjectByIdUseCase } from "../../use-cases/projects/get-project-by-id"
+
+import { checkIfIdIsValid } from "../../helpers/validation-id"
+
+import { ZodError } from "zod"
+import { ProjectNotFoundError } from "../../errors/project"
+import { badRequest, internalServerError } from "../../errors"
+
+import { getProjectByIdSchema } from "../../schemas/projects"
+
+export class GetProjectByIdController {
+  constructor(private getProjectByIdUseCase: GetProjectByIdUseCase) {
+    this.getProjectByIdUseCase = getProjectByIdUseCase
+  }
+
+  async execute(projectId: string) {
+    try {
+      const isIdValid = checkIfIdIsValid(projectId)
+
+      if (!isIdValid) {
+        return badRequest(
+          "The provided id is not valid. Please provide a valid id.",
+        )
+      }
+
+      await getProjectByIdSchema.parseAsync({ id: projectId })
+
+      const project = await this.getProjectByIdUseCase.execute(projectId)
+
+      return {
+        statusCode: 200,
+        body: project,
+      }
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest(error.errors[0].message)
+      }
+
+      if (error instanceof ProjectNotFoundError) {
+        return badRequest(error.message)
+      }
+
+      console.log(error)
+      return internalServerError("Internal server error")
+    }
+  }
+}
