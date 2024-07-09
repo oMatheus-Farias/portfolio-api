593ff158da534f5de075799b98dc536e7c6ef732
Matheus <matheusfariasdasneves@gmail.com>
2024-07-09 10:47:21 -0300

    refactor: refactor updateProjectSchema

--- src/schemas/projects.ts
+++ src/schemas/projects.ts
@@ -95,13 +95,6 @@ export const getByIdSchema = z.object({
 })
 
 export const updateProjectSchema = z.object({
-  userId: z
-    .string({
-      required_error: "User ID is required.",
-    })
-    .uuid({
-      message: "User ID must be a valid UUID.",
-    }),
   projectId: z
     .string({
       required_error: "ID is required.",
@@ -109,53 +102,11 @@ export const updateProjectSchema = z.object({
     .uuid({
       message: "ID must be a valid UUID.",
     }),
-  description: z
+  userId: z
     .string({
-      required_error: "Description is required.",
+      required_error: "User ID is required.",
     })
-    .trim()
-    .min(1, {
-      message: "Description is required.",
-    })
-    .max(500, {
-      message: "Description must be at most 500 characters.",
-    }),
-  imagesUrl: z
-    .string({
-      required_error: "Images URL is required.",
-    })
-    .array()
-    .nonempty({
-      message: "Images URL is required.",
-    }),
-  repositoryUrl: z
-    .string({
-      required_error: "Repository URL is required.",
-    })
-    .url({
-      message: "Please provide a valid URL.",
-    })
-    .trim()
-    .min(1, {
-      message: "Repository URL is required.",
-    }),
-  projectUrl: z
-    .string({
-      required_error: "Repository URL is required.",
-    })
-    .url({
-      message: "Please provide a valid URL.",
-    })
-    .trim()
-    .min(1, {
-      message: "Repository URL is required.",
-    }),
-  technologies: z
-    .string({
-      required_error: "Technologies is required.",
-    })
-    .array()
-    .nonempty({
-      message: "Technologies is required.",
+    .uuid({
+      message: "User ID must be a valid UUID.",
     }),
 })
