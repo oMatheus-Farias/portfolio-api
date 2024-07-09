ea45debcc70b3ba29d031791d6b633b9e1be2b61
Matheus <matheusfariasdasneves@gmail.com>
2024-07-09 11:14:02 -0300

    feat: add DeleteProjectUseCase

---
+++ src/use-cases/projects/delete-project.ts
@@ -0,0 +1,37 @@
+import { ProjectNotFoundError, UserUnauthorizedError } from "../../errors"
+import {
+  PostgresDeleteProjectRepository,
+  PostgresGetProjectByIdRepository,
+} from "../../repositories/postgres"
+
+export class DeleteProjectUseCase {
+  constructor(
+    private postgresDeleteProjectRepository: PostgresDeleteProjectRepository,
+    private postgresGetProjectByIdRepository: PostgresGetProjectByIdRepository,
+  ) {
+    this.postgresDeleteProjectRepository = postgresDeleteProjectRepository
+    this.postgresGetProjectByIdRepository = postgresGetProjectByIdRepository
+  }
+
+  async execute(projectId: string, userId: string) {
+    const projectAlreadyExists =
+      await this.postgresGetProjectByIdRepository.execute(projectId)
+
+    if (!projectAlreadyExists) {
+      throw new ProjectNotFoundError()
+    }
+
+    const userAuthorized = projectAlreadyExists.userId === userId
+
+    if (!userAuthorized) {
+      throw new UserUnauthorizedError()
+    }
+
+    const projectDeleted = await this.postgresDeleteProjectRepository.execute(
+      projectId,
+      userId,
+    )
+
+    return projectDeleted
+  }
+}
