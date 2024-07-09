666f465b91909056c85715f849c8e567479549b6
Matheus <matheusfariasdasneves@gmail.com>
2024-07-09 11:26:46 -0300

    feat: add DeleteProjectController

---
+++ src/controllers/projects/delete-project.ts
@@ -0,0 +1,63 @@
+import { DeleteProjectUseCase } from "../../use-cases"
+
+import {
+  internalServerError,
+  notFound,
+  ProjectNotFoundError,
+  unauthorized,
+  UserUnauthorizedError,
+} from "../../errors"
+import { ZodError } from "zod"
+
+import { checkIfIdIsValid } from "../../helpers/validation-id"
+import { updateAndDeleteProjectSchema } from "../../schemas/projects"
+
+export class DeleteProjectController {
+  constructor(private deleteProjectUseCase: DeleteProjectUseCase) {
+    this.deleteProjectUseCase = deleteProjectUseCase
+  }
+
+  async execute(projectId: string, userId: string) {
+    try {
+      await updateAndDeleteProjectSchema.parseAsync({ projectId, userId })
+
+      const projectIdIsValid = checkIfIdIsValid(projectId)
+      const userIdIsValid = checkIfIdIsValid(userId)
+
+      if (!projectIdIsValid) {
+        return notFound(
+          "Project ID is not valid. Please provide a valid project ID.",
+        )
+      }
+
+      if (!userIdIsValid) {
+        return notFound("User ID is not valid. Please provide a valid user ID.")
+      }
+
+      const projectDeleted = await this.deleteProjectUseCase.execute(
+        projectId,
+        userId,
+      )
+
+      return {
+        statusCode: 200,
+        body: projectDeleted,
+      }
+    } catch (error) {
+      if (error instanceof ProjectNotFoundError) {
+        return notFound(error.message)
+      }
+
+      if (error instanceof UserUnauthorizedError) {
+        return unauthorized(error.message)
+      }
+
+      if (error instanceof ZodError) {
+        return notFound(error.errors[0].message)
+      }
+
+      console.log(error)
+      return internalServerError("Internal server error.")
+    }
+  }
+}
