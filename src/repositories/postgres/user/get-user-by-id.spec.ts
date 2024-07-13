4bfa10da67dc39b515f848a9c16fb5b15ae295a7
Matheus <matheusfariasdasneves@gmail.com>
2024-07-13 18:40:33 -0300

    test: ensure get user by id on database

---
+++ src/repositories/postgres/user/get-user-by-id.spec.ts
@@ -0,0 +1,45 @@
+import { describe, it, expect, beforeEach } from "vitest"
+import { prisma } from "../../../app"
+
+import { PostgresGetUserByIdRepository } from "./get-user-by-id"
+
+import { faker } from "@faker-js/faker"
+
+describe("Postgres Get User By Id Repository", () => {
+  beforeEach(async () => {
+    await prisma.user.deleteMany()
+  })
+
+  const createUserParams = {
+    id: faker.string.uuid(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password({
+      length: 8,
+    }),
+  }
+
+  const makeSut = () => {
+    const sut = new PostgresGetUserByIdRepository()
+
+    return {
+      sut,
+    }
+  }
+
+  it("should get user by id on database", async () => {
+    const { sut } = makeSut()
+
+    const user = await prisma.user.create({
+      data: createUserParams,
+    })
+
+    const response = await sut.execute(user.id)
+
+    expect(response).not.toBeUndefined()
+    expect(response.firstName).toBe(user.firstName)
+    expect(response.lastName).toBe(user.lastName)
+    expect(response.email).toBe(user.email)
+  })
+})
