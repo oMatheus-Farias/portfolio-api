62dfee35524cdb0ceb3ca8d8de15efe1cc8a0f99
Matheus <matheusfariasdasneves@gmail.com>
2024-07-13 14:50:56 -0300

    test: ensure successfully create a project

---
+++ src/use-cases/projects/create-project.spec.ts
@@ -0,0 +1,59 @@
+import { describe, it, expect, vitest } from "vitest"
+
+import { CreateProjectUseCase } from "./create-project"
+
+import { faker } from "@faker-js/faker"
+
+describe("Create Project UseCase", () => {
+  const project = {
+    id: faker.string.uuid(),
+    name: faker.person.firstName(),
+    description: faker.lorem.paragraph(),
+    imagesUrl: [faker.internet.url()],
+    repositoryUrl: faker.internet.url(),
+    projectUrl: faker.internet.url(),
+    technologies: [faker.lorem.word()],
+    userId: faker.string.uuid(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+  }
+
+  class PostgresCreateProjectRepositoryStub {
+    async execute() {
+      return project
+    }
+  }
+
+  class PostgresGetProjectByNameRepositoryStub {
+    async execute() {
+      return null
+    }
+  }
+
+  const makeSut = () => {
+    const postgresCreateProjectRepositoryStub =
+      new PostgresCreateProjectRepositoryStub()
+    const postgresGetProjectByNameRepositoryStub =
+      new PostgresGetProjectByNameRepositoryStub()
+
+    const sut = new CreateProjectUseCase(
+      postgresCreateProjectRepositoryStub,
+      postgresGetProjectByNameRepositoryStub,
+    )
+
+    return {
+      sut,
+      postgresCreateProjectRepositoryStub,
+      postgresGetProjectByNameRepositoryStub,
+    }
+  }
+
+  it("should successfully create a project", async () => {
+    const { sut } = makeSut()
+
+    const result = await sut.execute({ params: project })
+
+    expect(result).not.toBeNull()
+    expect(result).toEqual(project)
+  })
+})
