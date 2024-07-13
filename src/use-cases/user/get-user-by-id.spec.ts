3beec1166079a27a8a9bc9e444ff4573f2c7821c
Matheus <matheusfariasdasneves@gmail.com>
2024-07-13 14:43:24 -0300

    test: ensure throw UserNotFoundError if user is not found

--- src/use-cases/user/get-user-by-id.spec.ts
+++ src/use-cases/user/get-user-by-id.spec.ts
@@ -3,6 +3,7 @@ import { describe, it, expect, vitest } from "vitest"
 import { GetUserByIdUseCase } from "./get-user-by-id"
 
 import { faker } from "@faker-js/faker"
+import { UserNotFoundError } from "../../errors"
 
 describe("Get User By Id UseCase", () => {
   const user = {
@@ -38,5 +39,17 @@ describe("Get User By Id UseCase", () => {
 
     expect(result).not.toBeNull()
     expect(result).toEqual(user)
+  })
+
+  it("should throw UserNotFoundError if user is not found", async () => {
+    const { sut, postgresGetUserByIdRepositoryStub } = makeSut()
+
+    vitest
+      .spyOn(postgresGetUserByIdRepositoryStub, "execute")
+      .mockRejectedValueOnce(new UserNotFoundError())
+
+    const result = sut.execute(user.id)
+
+    await expect(result).rejects.toThrowError(new UserNotFoundError())
   })
 })
