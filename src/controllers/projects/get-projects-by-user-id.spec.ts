5565745ca42e9cc9f4141c82bec9be3324cb98fa
Matheus <matheusfariasdasneves@gmail.com>
2024-07-14 12:27:05 -0300

    test: ensure return 500 if any error occurs

--- src/controllers/projects/get-projects-by-user-id.spec.ts
+++ src/controllers/projects/get-projects-by-user-id.spec.ts
@@ -97,4 +97,16 @@ describe("Get Projects By User Id Controller", () => {
 
     expect(result.statusCode).toBe(404)
   })
+
+  it("should return 500 if any error occurs", async () => {
+    const { sut, postgresGetProjectsByUserIdRepositoryStub } = makeSut()
+
+    vitest
+      .spyOn(postgresGetProjectsByUserIdRepositoryStub, "execute")
+      .mockRejectedValueOnce(new Error())
+
+    const result = await sut.execute(user.id)
+
+    expect(result.statusCode).toBe(500)
+  })
 })
