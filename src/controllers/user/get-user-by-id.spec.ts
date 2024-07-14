fcffd96a855c6b53ff89ac3ed5052c3035828290
Matheus <matheusfariasdasneves@gmail.com>
2024-07-14 11:02:35 -0300

    test: ensure return 400 if id is missing

--- src/controllers/user/get-user-by-id.spec.ts
+++ src/controllers/user/get-user-by-id.spec.ts
@@ -54,4 +54,12 @@ describe("Get User By Id Controller", () => {
 
     expect(result.statusCode).toBe(400)
   })
+
+  it("should return 400 if id is missing", async () => {
+    const { sut } = makeSut()
+
+    const result = await sut.execute("")
+
+    expect(result.statusCode).toBe(400)
+  })
 })
