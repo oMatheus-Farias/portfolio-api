4a98b75c4b07f419a04d33a36d73808fe5871537
Matheus <matheusfariasdasneves@gmail.com>
2024-07-14 11:52:17 -0300

    test: ensure return 200 if project is found

---
+++ src/controllers/projects/get-project-by-id.spec.ts
@@ -0,0 +1,54 @@
+import { describe, it, expect, vitest } from "vitest"
+
+import { GetProjectByIdController } from "./get-project-by-id"
+import { GetProjectByIdUseCase } from "../../use-cases/projects/get-project-by-id"
+
+import { faker } from "@faker-js/faker"
+
+describe("Get Project By Id Controller", () => {
+  const project = {
+    id: faker.string.uuid(),
+    name: faker.person.firstName(),
+    description: faker.lorem.paragraph(),
+    imagesUrl: [faker.internet.url()],
+    repositoryUrl: faker.internet.url(),
+    projectUrl: faker.internet.url(),
+    technologies: [faker.lorem.word()],
+    userId: faker.string.uuid(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+  }
+
+  class PostgresGetProjectByIdRepositoryStub {
+    async execute() {
+      return project
+    }
+  }
+
+  const makeSut = () => {
+    const postgresGetProjectByIdRepositoryStub =
+      new PostgresGetProjectByIdRepositoryStub()
+
+    const getProjectByIdUseCase = new GetProjectByIdUseCase(
+      postgresGetProjectByIdRepositoryStub,
+    )
+
+    const sut = new GetProjectByIdController(getProjectByIdUseCase)
+
+    return {
+      sut,
+      postgresGetProjectByIdRepositoryStub,
+    }
+  }
+
+  it("should return 200 if project is found", async () => {
+    const { sut } = makeSut()
+
+    const result = await sut.execute(project.id)
+
+    expect(result).toEqual({
+      statusCode: 200,
+      body: project,
+    })
+  })
+})
