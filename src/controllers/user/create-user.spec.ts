0e330d39cea35a6e516a84ed63b80237af552b6c
Matheus <matheusfariasdasneves@gmail.com>
2024-07-14 10:39:37 -0300

    test: ensure return error 400 if missing fields

---
+++ src/controllers/user/create-user.spec.ts
@@ -0,0 +1,77 @@
+import { describe, it, expect, vitest } from "vitest"
+
+import { CreateUserController } from "./create-user"
+import { CreateUserUseCase } from "../../use-cases"
+
+import { faker } from "@faker-js/faker"
+import { ZodError } from "zod"
+
+describe("Create User Controller", () => {
+  const user = {
+    id: faker.string.uuid(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  }
+
+  class PostgresCreateUserRepositoryStub {
+    async execute() {
+      return user
+    }
+  }
+
+  class PostgresGetUserByEmailRepositoryStub {
+    async execute() {
+      return null
+    }
+  }
+
+  class PasswordHasherAdapterStub {
+    async execute() {
+      return faker.string.uuid()
+    }
+  }
+
+  const makeSut = () => {
+    const postgresCreateUserRepositoryStub =
+      new PostgresCreateUserRepositoryStub()
+    const postgresGetUserByEmailRepositoryStub =
+      new PostgresGetUserByEmailRepositoryStub()
+    const passwordHasherAdapterStub = new PasswordHasherAdapterStub()
+
+    const createUserUseCaseStub = new CreateUserUseCase(
+      postgresCreateUserRepositoryStub,
+      postgresGetUserByEmailRepositoryStub,
+      passwordHasherAdapterStub,
+    )
+
+    const sut = new CreateUserController(createUserUseCaseStub)
+
+    return {
+      sut,
+      createUserUseCaseStub,
+    }
+  }
+
+  it("should return 201 with created user", async () => {
+    const { sut } = makeSut()
+
+    const response = await sut.execute({ httRequest: user })
+
+    expect(response).toEqual({
+      statusCode: 201,
+      body: user,
+    })
+  })
+
+  it("should return error 400 if missing fields", async () => {
+    const { sut } = makeSut()
+
+    const result = await sut.execute({
+      httRequest: null,
+    })
+
+    expect(result.statusCode).toBe(400)
+  })
+})
