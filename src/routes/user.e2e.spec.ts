af55811c1ecf64d7a5f7d355ba2deab6953596a3
Matheus <matheusfariasdasneves@gmail.com>
2024-07-15 20:35:26 -0300

    test: test e2e ensure POST /api/user/auth should return 200 when user is authenticated

--- src/routes/user.e2e.spec.ts
+++ src/routes/user.e2e.spec.ts
@@ -6,21 +6,6 @@ import { app } from "../app"
 import { faker } from "@faker-js/faker"
 
 describe("User routes E2E tests", () => {
-  it("POST /api/user should return 201 when user is created", async () => {
-    const response = await request(app)
-      .post("/api/user")
-      .send({
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password({
-          length: 8,
-        }),
-      })
-
-    expect(response.status).toBe(201)
-  })
-
   it("GET /api/user/email should return 200 when user is found", async () => {
     const { body } = await request(app)
       .post("/api/user")
@@ -62,5 +47,36 @@ describe("User routes E2E tests", () => {
       .set("Authorization", `Bearer ${token}`)
 
     expect(_response.status).toBe(200)
+  })
+
+  it("POST /api/user should return 201 when user is created", async () => {
+    const response = await request(app)
+      .post("/api/user")
+      .send({
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({
+          length: 8,
+        }),
+      })
+
+    expect(response.status).toBe(201)
+  })
+
+  it("POST /api/user/auth should return 200 when user is authenticated", async () => {
+    await request(app).post("/api/user").send({
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: "teste@teste.com",
+      password: "12345678",
+    })
+
+    const response = await request(app).post("/api/user/auth").send({
+      email: "teste@teste.com",
+      password: "12345678",
+    })
+
+    expect(response.status).toBe(200)
   })
 })
