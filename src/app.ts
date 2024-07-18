17d7ca05aa30f5637abf3db047eae9fdb458b436
Matheus <matheusfariasdasneves@gmail.com>
2024-07-18 11:36:31 -0300

    chore: add swagger config

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,12 @@ import express from "express"
 import cors from "cors"
 import { router } from "./routes/routes"
 
+import fs from "fs"
+import path from "path"
+
 import { PrismaClient } from "@prisma/client"
+
+import swaggerUi from "swagger-ui-express"
 
 export const app = express()
 
@@ -11,3 +16,9 @@ export const prisma = new PrismaClient()
 app.use(cors())
 app.use(express.json())
 app.use(router)
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../docs/swagger.json"), "utf8"),
+)
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
